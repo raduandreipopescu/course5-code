0349c541275f20fa9df2d4a73c2ae2ae7b5359da
raduandreipopescu <popescu.a.radu@gmail.com>
2023-01-03 20:39:29 +0200

    Added new line

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,7 @@
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Something");
+        System.out.println("Something else");
+        System.out.println("Something more");
     }
 }
